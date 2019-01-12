6973ff04f7a14f325a7e8fb491872445f2ed936e
Diksha65 <agarwaldiksha6596@gmail.com>
2019-01-12 19:45:00 +0530

    Added viewpager section

--- app/src/main/java/com/example/diksha/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/diksha/bottomnavigation/MainActivity.kt
@@ -5,12 +5,8 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import android.support.annotation.NonNull
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
-import android.view.MenuItem
-import android.support.v4.view.ViewPager
-import android.util.Log
 import java.util.ArrayList
 
 
